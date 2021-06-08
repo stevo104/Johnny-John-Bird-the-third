aa8f7e09e6ca3b1a985cc8ad132009609de68f9e
stevo104 <85533272+stevo104@users.noreply.github.com>
2021-06-08 12:10:39 +1000

    Create DoorSwitch.cs

---
+++ DoorSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DoorSwitch : MonoBehaviour
+  
+{
+    public GameObject bigDoor;
+    public GameObject openDoorText;
+    public AnimationClip doorOpen;
+    public AnimationClip doorClose;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {// Activating the door open, 
+        if
+           (Input.GetKeyDown(KeyCode.F))
+        {
+            bigDoor.SetActive(!bigDoor.activeSelf);
+            doorOpen = GameObject.Find = (bigDoor);
+            
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        openDoorText.SetActive(true);
+        openDoorText.GetComponent<Text>().text = "Press F to open the door";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+    

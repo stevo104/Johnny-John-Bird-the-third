022bdf7cfd67f06434668ca9e4e1041522ae78fe
stevo104 <85533272+stevo104@users.noreply.github.com>
2021-06-16 11:28:21 +1000

    Create SwitchTrigger.cs

---
+++ SwitchTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTrigger : MonoBehaviour
+{
+    public bool withinrange;
+    private doorScript Bigdoor;
+
+    private void update()
+    {
+        if (withinrange && Bigdoor != null && Input.GetKeyDown(KeyCode.F))
+        {
+            Bigdoor.DoorOpen();
+        }
+    }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Bigdoor = other.GetComponent<doorScript>();
+            if (Bigdoor != null)
+            {
+                withinrange = true;
+            }
+        }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name.Contains("Door"))
+        {
+            withinrange = false;
+            Bigdoor = null;
+        }
+    }
+}
+    

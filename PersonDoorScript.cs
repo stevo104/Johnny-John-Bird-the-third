c66bc04e23662929da60b3bed5665eae832e30d4
stevo104 <85533272+stevo104@users.noreply.github.com>
2021-06-21 15:22:40 +1000

    Create PersonDoorScript.cs
    I had to break the two scripts up as having it in one did not work

---
+++ PersonDoorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonDoorScript : MonoBehaviour
+{ 
+    private bool inRange;
+    private doorScript BigDoor;
+
+    private void Update()
+    {
+        if(inRange && BigDoor != null && Input.GetKeyDown(KeyCode.F))
+        {
+            BigDoor.DoorOpen();
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BigDoor = other.GetComponent<doorScript>();
+        if (BigDoor != null)
+        {
+            inRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name.Contains("BigDoor"))
+        {
+            inRange = false;
+            BigDoor = null;
+        }
+    }
+    
+    
+}
+
+

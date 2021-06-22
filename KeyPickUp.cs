6b3817bbf835f8c209ae37f6ecc7ec5942aebd02
stevo104 <85533272+stevo104@users.noreply.github.com>
2021-06-22 11:12:27 +1000

    Create KeyPickUp.cs
    created a key pick up script, tested and doesn't work

---
+++ KeyPickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickUp : MonoBehaviour
+{
+    public GameObject Key;
+    private bool inRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Key.SetActive(true);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(inRange = true); if (inRange && Key!=null && Input.GetKeyDown(KeyCode.F))
+        {
+            Destroy(Key);
+        }
+    }
+}
+    

02c83427288f7dc459c40e3c1f5a0b2f55d86097
stevo104 <85533272+stevo104@users.noreply.github.com>
2021-06-22 10:12:37 +1000

    Create UiSwitch.cs
    Had to create a Separate script for UI in the level

---
+++ UiSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiSwitch : MonoBehaviour
+{
+
+    public GameObject uiObject;
+
+    void Start()
+     // Set the UI Text Object to false so it does not show up on screen
+    {
+        uiObject.SetActive(false);
+    }
+   
+    void OnTriggerEnter(Collider player)
+        // When the player enters the trigger it will check for the tag, if successful it will display the UI Object on Screen
+    {
+        if (player.gameObject.tag == "Johnny")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+        //Once player has left the collider the UI object will be destroyed
+        IEnumerator WaitForSec()
+        {
+            yield return new WaitForSeconds(5);
+            Destroy(uiObject);
+            Destroy(gameObject);
+
+        }
+    }
+}

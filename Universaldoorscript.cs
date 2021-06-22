b51e6eb603fcb76d3bd4c5ae370f4d8ab93fd836
stevo104 <85533272+stevo104@users.noreply.github.com>
2021-06-22 12:23:01 +1000

    Update Universaldoorscript.cs
    Final iteration of the code

--- Universaldoorscript.cs
+++ Universaldoorscript.cs
@@ -5,27 +5,27 @@ using DG.Tweening;
 
 public class Universaldoorscript : MonoBehaviour
 {
+    //The Different Varaiables within the Script, only 3 of them can be edited in Unity, the rest are permanent.
     public float openSpace;
     public float time;
     public GameObject Switch;
     public GameObject bigDoor;
     public GameObject uiObject;
     private Vector3 closepos;
-    
-
-    // Start is called before the first frame update
+  
+ // at the start, the script will have the UI pop up set to "off" and the "Big door" will be closed.
     void Start()
     {
         uiObject.SetActive(false);
         closepos = bigDoor.transform.localPosition;
     }
- 
     public void DoorOpen()
+ // This is the function that causes the door to move along the "X Axis", will only happen when called upon however. 
     {
         bigDoor.transform.DOLocalMoveX(closepos.x + openSpace, 1);
     }
     void OnTriggerEnter(Collider player)
-    // When the player enters the trigger it will check for the tag, if successful it will display the UI Object on Screen
+ // When the player enters the trigger it will check for the tag, if successful it will display the UI Object on Screen and trigger the countdown to begin.
     {
         if (player.gameObject.tag == "Johnny")
         {
@@ -33,12 +33,11 @@ public class Universaldoorscript : MonoBehaviour
             StartCoroutine("WaitSec");
         }
     }
-
     private IEnumerator WaitSec()
+ // When the countdown reaches zero the UI text will dissapear from the screen, until the collider is walked into again.
     {
         yield return new WaitForSeconds(2);
         uiObject.SetActive(false);
-
     }
-        }
+}
 

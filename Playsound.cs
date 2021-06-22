9b0192c909e80c7078a25a9c8b2603d5895cf961
stevo104 <85533272+stevo104@users.noreply.github.com>
2021-06-22 16:12:57 +1000

    Create Playsound.cs

---
+++ Playsound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playsound : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    AudioSource soundSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        soundSource.PlayOneShot(SoundToPlay);
+    }
+}
